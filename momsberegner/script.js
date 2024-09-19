72de55ca7b307cab3090353db1993787133caccc
Sara <sala0002@stud.kea.dk>
2024-09-19 11:58:59 +0200

    hej

--- momsberegner/script.js
+++ momsberegner/script.js
@@ -1,5 +1,7 @@
 document.getElementById("beregnBtn").addEventListener("click", beregn);
 
+// her er beregningen, som løses ved at lægge momsen til beløbet
+// hvis ikke der bliver skrevet noget i moms input er standardværdien 2%
 function beregnMoms(beloeb, moms = 2) {
   let total = beloeb + beloeb * (moms / 100);
   return total;
@@ -12,12 +14,15 @@ function beregn() {
   if (moms === "") {
     moms = 2;
   }
-
+  // parseFloat skrives for at sikre at værdierne kommer ud som tal
   let totalBeloeb = beregnMoms(parseFloat(beloeb), parseFloat(moms));
   document.getElementById("resultat").textContent = `Beløbet med ${moms}% moms lagt til er: ${totalBeloeb} kr.`;
 }
 
-// 	1.	Inputfelterne: Brugeren indtaster et beløb og evt en momssats
-//  2.   Klik på knappen: Når knappen klikkes, bliver funktionen beregn() kaldt
-//  3.	Beregn moms: Funktionen henter værdierne fra inputfelterne, tjekker, om moms er tomt, og kalder       beregnMoms-funktionen
+// 1. Inputfelterne: Brugeren indtaster et beløb og evt en momssats
+
+// 2. Klik på knappen: Når knappen klikkes, bliver funktionen beregn() kaldt
+
+// 3.	Beregn moms: Funktionen henter værdierne fra inputfelterne, tjekker, om moms er tomt, og kalder       beregnMoms-funktionen
+
 // 4.	Vis resultat: Det beregnede beløb bliver vist på siden ved hjælp af document.getElementById('resultat').textContent, der opdaterer teksten i HTML
